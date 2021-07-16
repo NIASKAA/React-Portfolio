2bdcc0742b276b73d6debe011530872ae21628fc
NIASKA <mkman751@yahoo.com>
2021-07-15 19:50:11 -0500

    added navbar and footer

---
+++ src/Components/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+    return (
+        <div className="text-center text-dark p-1">
+            © 2021 Copyright NIASKA Productions
+        </div>
+    )
+}
+
+export default Footer
